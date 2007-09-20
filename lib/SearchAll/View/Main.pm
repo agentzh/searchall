cd56c212e9cdf9a95871c47fa83d9573b99a5cd3
agentz <agentz@1fb3952c-a762-dc11-853b-000bcdefe56b>
2007-09-20 12:36:54 +0000

    r1649@agentz-office (orig r781):  (no author) | 2007-09-20 20:32:36 +0800  added persist support for checkboxes in both views
    git-svn-id: http://svn.openfoundry.org/searchall/trunk@40 1fb3952c-a762-dc11-853b-000bcdefe56b

--- lib/SearchAll/View/Main.pm
+++ lib/SearchAll/View/Main.pm
@@ -26,7 +26,7 @@ template main => sub {
             #'xmlns:html' => $::HTML_NAME_SPACE,
             width => 800,
             height => 600,
-            persist => "width height",
+            persist => "screenX screenY width height",
         }
 
 	show 'searchall';
