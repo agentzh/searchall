eba8ea9ce30e46c8da4966dcac1e52ee0fd0334e
agentz <agentz@1fb3952c-a762-dc11-853b-000bcdefe56b>
2007-09-28 13:02:51 +0000

    r1876@agentz-office (orig r838):  (no author) | 2007-09-28 21:01:42 +0800  bumped the version number to 0.1.1
    git-svn-id: http://svn.openfoundry.org/searchall/trunk@97 1fb3952c-a762-dc11-853b-000bcdefe56b

--- lib/SearchAll/App.pm
+++ lib/SearchAll/App.pm
@@ -43,7 +43,7 @@ use XUL::App schema {
         xpifile 'searchall.xpi' =>
             id is 'searchall@yahoo.cn',
             name is 'SearchAll',
-            version is '0.1.0',
+            version is '0.1.1',
             targets {
                 Firefox => ['2.0' => '3.0a5'],
                 Mozilla => ['1.5' => '1.8'],
