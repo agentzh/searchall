0385e0509cc216a7461afc4601102316ea916be9
agentz <agentz@1fb3952c-a762-dc11-853b-000bcdefe56b>
2007-10-26 09:49:17 +0000

    r2835@agentz-office (orig r551):  agentzh | 2007-10-26 17:57:27 +0800  searchall - added dom-logger.js
    git-svn-id: http://svn.openfoundry.org/searchall/trunk@319 1fb3952c-a762-dc11-853b-000bcdefe56b

---
+++ js/dom-logger.js
@@ -0,0 +1,32 @@
+if (typeof SearchAll == 'undefined') SearchAll = {};
+
+SearchAll.DomLogger = function (listWidget, textWidget) {
+    this.domTable = {};
+    this.listWidget = listWidget;
+    this.textWidget = textWidget;
+    var logger = this;
+    $(listWidget).select(function () {
+        //alert(this);
+        var label = this.selectedItem.label;
+        //alert(label);
+        textWidget.value = logger.domTable[label];
+    });
+};
+
+SearchAll.DomLogger.prototype.log = function (dom, desc) {
+    if (!desc) {
+        var time = new Date().getTime();
+        desc = time;
+    }
+    var label = desc;
+    var i = 1;
+    while (this.domTable[label]) {
+        i++;
+        label = desc + "-" + i;
+    }
+    this.listWidget.appendItem(label, label);
+    var str = SearchAll.Dom.dump(dom);
+    this.domTable[label] = str;
+    this.textWidget.value = str;
+}
+
