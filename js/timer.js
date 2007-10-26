209b99f4d0cdbd2cd620ad30c12cf8db0b4dfa41
agentz <agentz@1fb3952c-a762-dc11-853b-000bcdefe56b>
2007-10-26 09:51:02 +0000

    r2838@agentz-office (orig r552):  agentzh | 2007-10-26 17:59:51 +0800  searchall - cleaned up timer.js a bit more
    git-svn-id: http://svn.openfoundry.org/searchall/trunk@320 1fb3952c-a762-dc11-853b-000bcdefe56b

--- js/timer.js
+++ js/timer.js
@@ -11,6 +11,7 @@ SearchAll.Timer.prototype = {
     now: function (c) {
         return Date.now();
     },
+
     start: function (tag) {
         //alert("starting: " + tag);
         if (!this.tags[tag])
@@ -18,21 +19,23 @@ SearchAll.Timer.prototype = {
         var pair = [this.now()];
         this.tags[tag].unshift(pair);
     },
+
     stop: function (tag, params) {
         //alert("stopping: " + tag);
-        var end_time = this.now();
+        var endTime = this.now();
         var list = this.tags[tag];
         if (!list) return;
         var pair = list[0];
         if (!pair || pair.length > 1) {
             if (params.force && pair)
-                pair[1] = end_time;
+                pair[1] = endTime;
             else
                 error("Timer: time out of sync!");
             return;
         }
-        pair.push(end_time);
+        pair.push(endTime);
     },
+
     isTiming: function (tag) {
         var list = this.tags[tag];
         //Debug.JJJ(this);
@@ -41,6 +44,7 @@ SearchAll.Timer.prototype = {
         var pair = list[0];
         return pair && pair.length == 1;
     },
+
     lastResult: function (tag) { // in ms
         //alert("Here!");
         var list = this.tags[tag];
@@ -49,6 +53,7 @@ SearchAll.Timer.prototype = {
         //Debug.JJJ(this);
         return pair[1] - pair[0];
     },
+
     reset: function () {
         this.tags = {};
         return this;
