cf5552ba7438d45bb5ed639f7f0d0b010a17ec55
agentz <agentz@1fb3952c-a762-dc11-853b-000bcdefe56b>
2008-02-29 04:19:40 +0000

    r7649@agentz-office (orig r1849):  agentzh | 2008-02-29 12:15:45 +0800  [searchall] bumped the version number to 0.4.13
    git-svn-id: http://svn.openfoundry.org/searchall/trunk@491 1fb3952c-a762-dc11-853b-000bcdefe56b

--- lib/SearchAll/App.pm
+++ lib/SearchAll/App.pm
@@ -49,7 +49,7 @@ use XUL::App schema {
             id is 'searchall@yahoo.cn',
             name is 'SearchAll',
             description is 'A side-by-side search engine comparison tool',
-            version is '0.4.12',
+            version is '0.4.13',
             targets {
                 Firefox => ['2.0' => '3.0b4'],
                 Mozilla => ['1.5' => '1.8'],
